4f8f765e35eeb461a4e5410fd7da3aace227255d
Manuel Rodriguez <ro7josemanuel@gmail.com>
2023-10-04 19:28:56 +0200

    Applying liveData

--- app/src/main/java/com/manuelrodriguez/pokedex/MainActivity.kt
+++ app/src/main/java/com/manuelrodriguez/pokedex/MainActivity.kt
@@ -3,6 +3,7 @@ package com.manuelrodriguez.pokedex
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
@@ -20,10 +21,13 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.produceState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import coil.compose.AsyncImage
 import com.manuelrodriguez.pokedex.data.remote.responses.ServerPokemonResult
 import com.manuelrodriguez.pokedex.ui.PokemonsService
@@ -37,17 +41,9 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             PokedexAppTheme {
-                // A surface container using the 'background' color from the theme
 
-                val pokemons = produceState<List<ServerPokemonResult>>(initialValue = emptyList()) {
-                    value = Retrofit.Builder()
-                        .addConverterFactory(GsonConverterFactory.create())
-                        .baseUrl("https://pokeapi.co/api/v2/")
-                        .build()
-                        .create(PokemonsService::class.java)
-                        .getPokemonList(limit = 151, offset = 0)
-                        .results
-                }
+                val viewModel: MainViewModel = viewModel()
+                val state by viewModel.state.observeAsState(MainViewModel.UiState())
 
                 Surface(
                     modifier = Modifier.fillMaxSize(),
@@ -62,7 +58,7 @@ class MainActivity : ComponentActivity() {
                             verticalArrangement = Arrangement.spacedBy(4.dp),
                             contentPadding = PaddingValues(4.dp)
                         ) {
-                            items(pokemons.value) { pokemon ->
+                            items(state.pokemonList) { pokemon ->
 
                                 PokemonItem(pokemon)
                             }
--- app/src/main/java/com/manuelrodriguez/pokedex/MainViewModel.kt
+++ app/src/main/java/com/manuelrodriguez/pokedex/MainViewModel.kt
@@ -1,4 +1,36 @@
 package com.manuelrodriguez.pokedex
 
-class MainViewModel {
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.manuelrodriguez.pokedex.data.remote.responses.ServerPokemonResult
+import com.manuelrodriguez.pokedex.ui.PokemonsService
+import kotlinx.coroutines.launch
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class MainViewModel : ViewModel() {
+
+    private val _state = MutableLiveData(UiState())
+    var state: LiveData<UiState> = _state
+    init {
+        viewModelScope.launch {
+            _state.value = UiState(Retrofit.Builder()
+                .addConverterFactory(GsonConverterFactory.create())
+                .baseUrl("https://pokeapi.co/api/v2/")
+                .build()
+                .create(PokemonsService::class.java)
+                .getPokemonList(limit = 151, offset = 0)
+                .results)
+        }
+
+    }
+
+    data class UiState(
+        val pokemonList: List<ServerPokemonResult> = emptyList()
+    )
 }

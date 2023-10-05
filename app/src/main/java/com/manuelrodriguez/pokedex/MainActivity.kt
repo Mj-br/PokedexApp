de07c7f132712bccb90af3d81999a324633159b9
Manuel Rodriguez <ro7josemanuel@gmail.com>
2023-10-05 19:28:53 +0200

    Applying stateFlow

--- app/src/main/java/com/manuelrodriguez/pokedex/MainActivity.kt
+++ app/src/main/java/com/manuelrodriguez/pokedex/MainActivity.kt
@@ -3,8 +3,9 @@ package com.manuelrodriguez.pokedex
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.viewModels
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.aspectRatio
@@ -14,7 +15,11 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
@@ -23,18 +28,15 @@ import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.livedata.observeAsState
-import androidx.compose.runtime.produceState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import coil.compose.AsyncImage
 import com.manuelrodriguez.pokedex.data.remote.responses.ServerPokemonResult
-import com.manuelrodriguez.pokedex.ui.PokemonsService
 import com.manuelrodriguez.pokedex.ui.theme.PokedexAppTheme
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
 
 class MainActivity : ComponentActivity() {
     @OptIn(ExperimentalMaterial3Api::class)
@@ -43,7 +45,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             PokedexAppTheme {
 
-                val viewModel: MainViewModel = viewModel()
+                val viewModel: PokemonListViewModel = viewModel()
                 val state by viewModel.state.collectAsState()
 
                 Surface(
@@ -52,18 +54,32 @@ class MainActivity : ComponentActivity() {
                 ) {
                     Scaffold(topBar = { TopAppBar(title = { Text(text = "") }) }) { padding ->
 
-                        LazyVerticalGrid(
-                            columns = GridCells.Adaptive(120.dp),
-                            modifier = Modifier.padding(padding),
-                            horizontalArrangement = Arrangement.spacedBy(4.dp),
-                            verticalArrangement = Arrangement.spacedBy(4.dp),
-                            contentPadding = PaddingValues(4.dp)
-                        ) {
-                            items(state.pokemonList) { pokemon ->
+                        if (state.loading) {
+                            Box(
+                                modifier = Modifier.fillMaxSize(),
+                                contentAlignment = Alignment.Center
+                            ) {
+                                CircularProgressIndicator()
+                            }
 
-                                PokemonItem(pokemon)
+                        }
+                        if (state.pokemonList.isNotEmpty()) {
+                            LazyVerticalGrid(
+                                columns = GridCells.Adaptive(120.dp),
+                                modifier = Modifier.padding(padding),
+                                horizontalArrangement = Arrangement.spacedBy(4.dp),
+                                verticalArrangement = Arrangement.spacedBy(4.dp),
+                                contentPadding = PaddingValues(4.dp)
+                            ) {
+                                items(state.pokemonList) { pokemon ->
+
+                                    PokemonItem(
+                                        pokemon = pokemon,
+                                        onClick = { viewModel.onPokemonClick(pokemon) })
+                                }
                             }
                         }
+
 
                     }
 
@@ -75,21 +91,38 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun PokemonItem(pokemon: ServerPokemonResult) {
+fun PokemonItem(pokemon: ServerPokemonResult, onClick: () -> Unit) {
     val number = if (pokemon.url.endsWith("/")) {
         pokemon.url.dropLast(1).takeLastWhile { it.isDigit() }
     } else {
         pokemon.url.takeLastWhile { it.isDigit() }
     }
-    val imageUrl = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/$number.png"
+    val imageUrl =
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/$number.png"
 
-    Column {
-        AsyncImage(
-            model = imageUrl, contentDescription = pokemon.name,
-            modifier = Modifier
-                .fillMaxWidth()
-                .aspectRatio(2 / 3f)
-        )
+    Column(modifier = Modifier.clickable { onClick() }) {
+
+        Box() {
+            AsyncImage(
+                model = imageUrl, contentDescription = pokemon.name,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .aspectRatio(2 / 3f)
+            )
+
+            if (pokemon.favorite) {
+                Icon(
+                    imageVector = Icons.Default.Favorite,
+                    contentDescription = "",
+                    modifier = Modifier
+                        .align(Alignment.TopEnd)
+                        .padding(8.dp),
+                    tint = Color.Red
+                )
+            }
+        }
+
+
         Text(
             text = pokemon.name,
             modifier = Modifier.padding(16.dp),
--- app/src/main/java/com/manuelrodriguez/pokedex/data/remote/responses/ServerPokemonResult.kt
+++ app/src/main/java/com/manuelrodriguez/pokedex/data/remote/responses/ServerPokemonResult.kt
@@ -2,5 +2,6 @@ package com.manuelrodriguez.pokedex.data.remote.responses
 
 data class ServerPokemonResult(
     val name: String,
-    val url: String
+    val url: String,
+    val favorite: Boolean = false
 )
